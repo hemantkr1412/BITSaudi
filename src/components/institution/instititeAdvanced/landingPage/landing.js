ef2c5efdcfd8ffdc47425ecd9274ed26535b8a81
MaahiVohra <vohramaahi@gmail.com>
2023-06-27 15:06:54 +0530

    add: translation to sector nav & sidebar

--- src/components/institution/instititeAdvanced/landingPage/landing.js
+++ src/components/institution/instititeAdvanced/landingPage/landing.js
@@ -15,170 +15,206 @@ import Connect from "../../../connection/Connect";
 import KYC from "../../../kyc/kyc";
 import Subscription from "../subscription/subscription";
 import CertIssue from "../certIssue/certIssue";
-
+import { useTranslation } from "react-i18next";
 
 const InstitutesLandingPage = () => {
-  const user = useContext(UserContext);
-  const [isSidebar, setIsSidebar] = useState(false);
-  const [view, setView] = useState("education");
-  const [certData, setCertData] = useState(null);
-  const [category, setCategory] = useState("educational certificates");
-  
+	const user = useContext(UserContext);
+	const [isSidebar, setIsSidebar] = useState(false);
+	const [view, setView] = useState("education");
+	const [certData, setCertData] = useState(null);
+	const [category, setCategory] = useState("educational certificates");
+	const { t, i18n } = useTranslation();
+	const [sector, setSector] = useState({
+		textKey: "education",
+		text: t("sectors.education"),
+		logo: <SchoolIcon fontSize="large" />,
+	});
 
-  const [sector, setSector] = useState({
-    text: "Education",
-    logo: <SchoolIcon fontSize="large" />,
-  });
-  const sectors = [
-    {
-      text: "Education",
-      logo: <SchoolIcon fontSize="large" />,
-    },
-    {
-      text: "Event Management",
-      logo: <EmojiEventsIcon fontSize="large" />,
-    },
-    {
-      text: "Healthcare",
-      logo: <HealthAndSafetyIcon fontSize="large" />,
-    },
-    {
-      text: "Legal",
-      logo: <GavelIcon fontSize="large" />,
-    },
-    {
-      text: "Real Estate",
-      logo: <HomeIcon fontSize="large" />,
-    },
-    {
-      text: "Sports",
-      logo: <SportsHandballIcon fontSize="large" />,
-    },
-    {
-      text: "Entertainment",
-      logo: <CelebrationIcon fontSize="large" />,
-    },
-  ];
+	const sectors = [
+		{
+			textKey: "education",
+			text: t("sectors.education"),
+			logo: <SchoolIcon fontSize="large" />,
+		},
+		{
+			textKey: "eventManagement",
+			text: t("sectors.eventManagement"),
+			logo: <EmojiEventsIcon fontSize="large" />,
+		},
+		{
+			textKey: "healthcare",
+			text: t("sectors.healthcare"),
+			logo: <HealthAndSafetyIcon fontSize="large" />,
+		},
+		{
+			textKey: "legal",
+			text: t("sectors.legal"),
+			logo: <GavelIcon fontSize="large" />,
+		},
+		{
+			textKey: "realEstate",
+			text: t("sectors.realEstate"),
+			logo: <HomeIcon fontSize="large" />,
+		},
+		{
+			textKey: "sports",
+			text: t("sectors.sports"),
+			logo: <SportsHandballIcon fontSize="large" />,
+		},
+		{
+			textKey: "entertainment",
+			text: t("sectors.entertainment"),
+			logo: <CelebrationIcon fontSize="large" />,
+		},
+	];
+	useEffect(() => {
+		const handleLanguageChange = (newLanguage) => {
+			// Update the sector state with translated values
+			console.log(sector);
+			const translatedSector = {
+				textKey: sector.textKey,
+				text: t(`sectors.${sector.textKey}`),
+				logo: sector.logo,
+			};
+			setSector(translatedSector);
+		};
 
-  const Sidebar = () => {
-    const animation = isSidebar ? "sidebaropen" : "sidebarclose";
-    const width = isSidebar ? "300px" : "0%";
+		i18n.on("languageChanged", handleLanguageChange);
 
-    return (
-      <div
-        style={{
-          width: isSidebar ? "100%" : "0px",
-          backgroundColor: "var(--darkshade1)",
-          position: "absolute",
-          zIndex: 2,
-        }}
-        onClick={() => setIsSidebar(false)}
-      >
-        <div
-          className="institiutesidebar"
-          style={{
-            width: width,
-            height: "100vh",
-            transform: isSidebar ? "scaleX(100%)" : "scaleX(0%)",
-            overflowY: "scroll",
-            scrollbarWidth: "none",
-          }}
-        >
-          <div
-            style={{
-              fontSize: "1rem",
-              fontWeight: "800",
-              borderBottom: "1px solid var(--secondary)",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              textAlign: "center",
-              height: "50px",
-              padding: "0px 100px 0px 10px",
-            }}
-          >
-            <button
-              style={{ padding: "10px" }}
-              onClick={() => setIsSidebar(!isSidebar)}
-            >
-              X
-            </button>
-            Sectors
-          </div>
-          {sectors.map((sector, index) => (
-            <div
-              className="sector"
-              key={sector["text"] + "sector-for-Institution-Sidebar"}
-              onClick={() => {
-                setSector(sector);
-                setIsSidebar(false);
-              }}
-            >
-              {sector["logo"]}
-              {sector["text"]}
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  };
+		return () => {
+			// Clean up the event listener when the component unmounts
+			i18n.off("languageChanged", handleLanguageChange);
+		};
+	}, [sector, t, i18n]);
 
-  const Navbar = () => {
-    return (
-      <div className="institutenavbar" style={{ left: "0px", height: "50px" }}>
-        <MenuIcon
-          fontSize="large"
-          sx={{ margin: "0px 30px", "&:hover": { color: "var(--primary)" } }}
-          onClick={() => setIsSidebar(!isSidebar)}
-        />
-        {sector["logo"]}
-        {sector["text"]}
-      </div>
-    );
-  };
+	const Sidebar = () => {
+		const animation = isSidebar ? "sidebaropen" : "sidebarclose";
+		const width = isSidebar ? "300px" : "0%";
 
-  return (
-    <div className="institutepageadvanced">
-      <div style={{ display: "flex", width: "100%", height: "100%" }}>
-        <Sidebar />
-        <div
-          style={{ display: "flex", flexDirection: "column", width: "100%" }}
-        >
-          <Navbar />
+		return (
+			<div
+				style={{
+					width: isSidebar ? "100%" : "0px",
+					backgroundColor: "var(--darkshade1)",
+					position: "absolute",
+					zIndex: 2,
+				}}
+				onClick={() => setIsSidebar(false)}>
+				<div
+					className="institiutesidebar"
+					style={{
+						width: width,
+						height: "100vh",
+						transform: isSidebar ? "scaleX(100%)" : "scaleX(0%)",
+						overflowY: "scroll",
+						scrollbarWidth: "none",
+					}}>
+					<div
+						style={{
+							fontSize: "1rem",
+							fontWeight: "800",
+							borderBottom: "1px solid var(--secondary)",
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "space-between",
+							textAlign: "center",
+							height: "50px",
+							padding: "0px 100px 0px 10px",
+						}}>
+						<button
+							style={{ padding: "10px" }}
+							onClick={() => setIsSidebar(!isSidebar)}>
+							X
+						</button>
+						{t("sectors.heading")}
+					</div>
+					{sectors.map((sector, index) => (
+						<div
+							className="sector"
+							key={
+								sector["text"] +
+								"sector-for-Institution-Sidebar"
+							}
+							onClick={() => {
+								setSector(sector);
+								setIsSidebar(false);
+							}}>
+							{sector["logo"]}
+							{sector["text"]}
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	};
 
-          {!user.iswalletAvailable ? (
-            <NoWalletPage />
-          ) : !user.isConnected ? (
-            <Connect />
-          ) : user.userData.status !== "Approved" ? (
-            <KYC />
-          ) : (
-            <div
-              style={{
-                position: "absolute",
-                top: "50px",
-                left: "0px",
-                width: "100%",
-              }}
-            >
-              {view === "education" && (
-                <Education
-                  setView={setView}
-                  setCertData={setCertData}
-                  certData={certData}
-                  category={category}
-                  setCategory={setCategory}
-                />
-              )}
-              {view === "certIssue" && (
-                <CertIssue setView={setView} certData={certData} category={category}/>
-              )}
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	const Navbar = () => {
+		return (
+			<div
+				className="institutenavbar"
+				style={{ left: "0px", height: "50px" }}>
+				<MenuIcon
+					fontSize="large"
+					sx={{
+						margin: "0px 30px",
+						"&:hover": { color: "var(--primary)" },
+					}}
+					onClick={() => setIsSidebar(!isSidebar)}
+				/>
+				{sector["logo"]}
+				{sector["text"]}
+			</div>
+		);
+	};
+
+	return (
+		<div className="institutepageadvanced">
+			<div style={{ display: "flex", width: "100%", height: "100%" }}>
+				<Sidebar />
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						width: "100%",
+					}}>
+					<Navbar />
+
+					{!user.iswalletAvailable ? (
+						<NoWalletPage />
+					) : !user.isConnected ? (
+						<Connect />
+					) : user.userData.status !== "Approved" ? (
+						<KYC />
+					) : (
+						<div
+							style={{
+								position: "absolute",
+								top: "50px",
+								left: "0px",
+								width: "100%",
+							}}>
+							{view === "education" && (
+								<Education
+									setView={setView}
+									setCertData={setCertData}
+									certData={certData}
+									category={category}
+									setCategory={setCategory}
+								/>
+							)}
+							{view === "certIssue" && (
+								<CertIssue
+									setView={setView}
+									certData={certData}
+									category={category}
+								/>
+							)}
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default InstitutesLandingPage;
